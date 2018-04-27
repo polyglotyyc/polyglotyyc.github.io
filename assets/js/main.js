9d1fe028f5dc3182c253e7fde52e3407ba92f8a5
Tys von Gaza <tys.von.gaza@clio.com>
2018-04-27 12:42:11 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,9 @@ angular.module('Polyglot').directive("pgTickets", function() {
   return {
     restrict: "EA",
     replace: true,
-    template: "<center>\n  <a class=\"button\" onclick=\"showpass.tickets.eventPurchaseWidget('polyglot-yyc', {'theme-primary': '#dd3333', 'keep-shopping': false});return false;\" href=\"https://www.showpass.com/polyglot-yyc/\">Get Tickets Now</a>\n</center>"
+//    template: "<center>\n  <a class=\"button\" onclick=\"showpass.tickets.eventPurchaseWidget('polyglot-yyc', {'theme-primary': '#dd3333', 'keep-shopping': false});return false;\" href=\"https://www.showpass.com/polyglot-yyc/\">Get Tickets Now</a>\n</center>"
+    template: "<center>\n  <a class=\"button\" href=\"https://docs.google.com/forms/d/e/1FAIpQLSeMs1roEtoA-9jS7fKzKdZByHCbXtTBbuBfx_74DcbISECAKg/viewform\">Sold out: Join the Waiting List</a>\n</center>"
+    
   };
 });
 
